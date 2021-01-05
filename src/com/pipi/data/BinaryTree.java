86e9ce77490cffa1240d2943aa56ceda65083944
guanshengjun <guanshengjun@songguo7.com>
2021-01-05 20:29:54 +0800

    binary tree

--- src/com/pipi/data/BinaryTree.java
+++ src/com/pipi/data/BinaryTree.java
@@ -17,53 +17,122 @@ public class BinaryTree {
         }
     }
 
+    public TreeNode root;
+
+    public void insert(int val) {
+        TreeNode newNode = new TreeNode(val, null, null);
+        if (null == root) {
+            root = newNode;
+            return;
+        }
+
+        TreeNode node = root;
+
+        while (true) {
+            if (val < node.val) {
+                // left
+                if (node.left == null) {
+                    node.left = newNode;
+                    return;
+                }
+                node = node.left;
+            } else {
+                // right
+                if (node.right == null) {
+                    node.right = newNode;
+                    return;
+                }
+                node = node.right;
+            }
+        }
+
+    }
+
+    // 1、如果当前节点没有子节点，父节点直接将当前节点设置为null
+    // 2、如果当前节点只有一个子节点（左子节点或右子节点），将父节点将指向删除节点的引用指向删除节点的子节点
+    // 3、如果当前节点包含两个子节点，找到右子树最小节点，把这个节点替换到删除节点的位置
+    public void delete(int val) {
+        // TODO
+    }
+
+    public TreeNode search(int val) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode node = root;
+
+        while (node != null) {
+
+            if (node.val == val) {
+                return node;
+            }
+
+            node = val < node.val ? node.left : node.right;
+        }
+
+        return null;
+    }
+
+    public void preOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.val);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        preOrder(node.left);
+        System.out.println(node.val);
+        preOrder(node.right);
+    }
+
+    public void postOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        preOrder(node.left);
+        preOrder(node.right);
+        System.out.println(node.val);
+    }
+
     public static void main(String[] args) {
-        TreeNode left11 = new TreeNode(11, null, null);
-        TreeNode left12 = new TreeNode(12, null, null);
-        TreeNode left1 = new TreeNode(1, left11, left12);
 
-        TreeNode right11 = new TreeNode(21, null, null);
-        TreeNode right12 = new TreeNode(22, null, null);
-        TreeNode right1 = new TreeNode(2, right11, right12);
+        BinaryTree tree = new BinaryTree();
+        tree.insert(10);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(12);
+        tree.insert(11);
+        tree.insert(13);
+        tree.insert(15);
+        tree.insert(14);
+        tree.insert(16);
 
-        TreeNode root = new TreeNode(0, left1, right1);
+        tree.delete(10);
 
-        System.out.println("=== pre order ===");
-        preOrder(root);
+        System.out.println("=== pre order (node, left, right) ===");
+        tree.preOrder(tree.root);
 
-        System.out.println("=== in order ===");
-        inOrder(root);
+        System.out.println("=== in order (left, node, right) ===");
+        tree.inOrder(tree.root);
 
-        System.out.println("=== post order ===");
-        postOrder(root);
+        System.out.println("=== post order (left, right, node) ===");
+        tree.postOrder(tree.root);
 
-    }
+        System.out.println("=== value 2 on which floor？ ===");
+        System.out.println(tree.search(2));
 
-    public static void preOrder(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        System.out.println(node.val);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
 
-    public static void inOrder(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        preOrder(node.left);
-        System.out.println(node.val);
-        preOrder(node.right);
-    }
-
-    public static void postOrder(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        preOrder(node.left);
-        preOrder(node.right);
-        System.out.println(node.val);
     }
 
 
